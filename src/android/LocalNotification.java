2141a6082f92bbb365d21e4e8054cad499f1eb02
mogaembo <mogaembo@users.noreply.github.com>
2014-12-04 21:14:17 -0600

    Update LocalNotification.java

--- src/android/LocalNotification.java
+++ src/android/LocalNotification.java
@@ -19,7 +19,7 @@
     under the License.
 */
 
-package de.appplant.cordova.plugin.localnotification;
+package com.agmosoft.cordova.plugin.localnotification;
 
 import java.util.ArrayList;
 import java.util.Date;
